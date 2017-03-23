a75fa95ad2bb951df5e9925464b3cc9a8ab6fa8f
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-03-23 16:49:44 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Read Me
+
+## Getting started
+
+-`npm install`
+
+-`npm start`
+
---
+++ src/sample-data.js
@@ -0,0 +1,17 @@
+// This is just some sample data so you don't have to think of your own!
+module.exports = {
+  data1: {
+    name: 'Sample Data 1',
+    image: 'http://i.istockimg.com/file_thumbview_approve/36248396/5/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
+    desc: 'Description of Sample Data 1.',
+    status: 'available'
+  },
+  data2: {
+    name: 'Sample Data 2',
+    image: 'http://i.istockimg.com/file_thumbview_approve/36248396/5/stock-photo-36248396-blackened-cajun-sea-bass.jpg',
+    desc: 'Description of Sample Data 2.',
+    status: 'unavailable'
+  }
+
+  
+};
