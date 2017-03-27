eb16872c66106dfe29e09d0c0c9b2c3d796ddd4a
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-03-27 13:39:28 +0100

    set up the sides menu on the same page as the main menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './Header';
 import PageHeader from './PageHeader'
-import Mains from './Menu.js';
+import Mains from './Mains.js';
+import Sides from './Sides.js'
 import sampleMains from '../mains.js'
 import sampleSides from '../sides.js'
 
@@ -35,6 +36,16 @@ loadMenu() {
               Object
                 .keys(this.state.mains)
                 .map(key => <Mains key={key} index={key} details={this.state.mains[key]}/>)
+            }
+            </ul>
+        </div>
+         <div className="menu">
+            <h4>Sides</h4>
+            <ul className="menu-content">
+                {
+              Object
+                .keys(this.state.sides)
+                .map(key => <Sides key={key} index={key} details={this.state.sides[key]}/>)
             }
             </ul>
         </div>
