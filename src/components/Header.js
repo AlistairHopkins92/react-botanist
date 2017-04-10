84cf25bcb39637df5747ff8bd03a955536ba6224
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-04-10 14:43:37 +0100

    update to header and images

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ class Header extends React.Component {
         var details = this.props.title
         return (
           <header className="top">
+            <img className="flip" src={require('../css/images/five-stars.png')} role='presentation' />
             <h1>{details}</h1>
             <img className="banner" src={require('../css/images/five-stars.png')} role='presentation' />
           </header>
